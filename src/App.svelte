e945894c611a9871114d590fdf57fa5f379bb906
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2023-01-30 12:40:36 +0100

    added delete function, some css

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,53 @@
 <script>
-    let inputValue = '';
+  let inputValue = '';
+  let storedData = [];
   
-    function handleInput(event) {
-      inputValue = event.target.value;
+  function handleInput(event) {
+    inputValue = event.target.value;
+  }
+
+  function handleSubmit() {
+    storedData = JSON.parse(localStorage.getItem('data')) || [];
+    storedData.push(inputValue);
+    localStorage.setItem('data', JSON.stringify(storedData));
+    inputValue = '';
+  }
+
+  function handleDelete() {
+    storedData = JSON.parse(localStorage.getItem('data')) || [];
+    storedData.pop();
+    localStorage.setItem('data', JSON.stringify(storedData));
+  }
+
+  function handleLocalStorageLimit() {
+    console.log('Local storage size limit reached');
+  }
+
+  window.addEventListener('storage', event => {
+    if (event.key === 'data' && event.newValue.length > 1048576) {
+      handleLocalStorageLimit();
     }
+  });
+</script>
+
+<fieldset>
+<form>
+<input type="text" bind:value={inputValue} on:input={handleInput} />
+<button on:click|preventDefault={handleSubmit}>Send</button>
+<button on:click|preventDefault={handleDelete}>Delete</button>
+</form>
+
+
+<ol>
   
-    function handleSubmit() {
-      const storedData = JSON.parse(localStorage.getItem('data')) || [];
-      storedData.push(inputValue);
-      localStorage.setItem('data', JSON.stringify(storedData));
-      inputValue = '';
-      console.log(storedData);
-    }
+  {#each storedData as item}
+    <li>{item}</li>
+  {/each}
   
-    function handleLocalStorageLimit() {
-      console.log('Local storage size limit reached');
-    }
-  
-    window.addEventListener('storage', event => {
-      if (event.key === 'data' && event.newValue.length > 1048576) {
-        handleLocalStorageLimit();
-      }
-    });
-  </script>
-  
-  <input type="text" bind:value={inputValue} on:input={handleInput} />
-  <button on:click={handleSubmit}>Send</button>
-  
+</ol>
+</fieldset>
+
+<style>
+fieldset{position: fixed; top: 1em; left: 1em;}
+
+</style>
